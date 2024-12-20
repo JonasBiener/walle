e2ddcbffe66faf467259b54c218e09615bd6e7ad
JonasBiener <46632213+JonasBiener@users.noreply.github.com>
2024-12-21 00:21:07 +0100

    Added Svelte Webapp

---
+++ webapp/src/lib/connection.svelte.ts
@@ -0,0 +1,54 @@
+import * as bluetooth from "$lib/bluetooth"
+import { browser } from "$app/environment";
+
+let isConnectedState = $state(false);
+export const isConnected = () => isConnectedState;
+let isConnectingState = $state(false);
+export const isConnecting = () => isConnectingState;
+let isErrorState = $state(false);
+export const isError = () => isErrorState;
+let connectionMessageState = $state("Disconnected");
+export const connectionMessage = () => connectionMessageState;
+
+if (browser) {
+    isConnectedState = JSON.parse(sessionStorage.getItem("isConnected") || "{}");
+    isErrorState = JSON.parse(sessionStorage.getItem("isError") || "{}");
+}
+
+export async function performConnect(): Promise<void> {
+    try {
+        sessionStorage.setItem("isConnected", JSON.stringify(false));
+        sessionStorage.setItem("isError", JSON.stringify(false));
+        isConnectedState = false;
+        isConnectingState = true;
+        isErrorState = false;
+        connectionMessageState = "Discovering";
+        //await bluetooth.discoverBLE();
+        await new Promise(r => setTimeout(r, 500));
+        connectionMessageState = "Connecting";
+        //await bluetooth.connectBLE();
+        await new Promise(r => setTimeout(r, 500));
+        connectionMessageState = "Establishing";
+        //await bluetooth.discoverServicesAndCharacteristicsBLE();
+        await new Promise(r => setTimeout(r, 500));
+        isConnectedState = true;
+        isConnectingState = false;
+        connectionMessageState = "Connected";
+        sessionStorage.setItem("isConnected", JSON.stringify(true));
+    } catch (error: any) {
+        isConnectedState = false;
+        isConnectingState = false;
+        isErrorState = true;
+        connectionMessageState = error.message;
+        sessionStorage.setItem("isError", JSON.stringify(true));
+    }
+}
+
+export async function performDisconnect(): Promise<void> {
+    // await bluetooth.disconnectBLE();
+    await new Promise(r => setTimeout(r, 500));
+    isConnectedState = false;
+    isConnectingState = false;
+    connectionMessageState = "Disconnected";
+    sessionStorage.setItem("isConnected", JSON.stringify(false));
+}
---
+++ webapp/src/routes/control/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { goto } from "$app/navigation";
+    import Wrapper from "../wrapper.svelte";
+    import { performDisconnect, isConnected } from "$lib/connection.svelte";
+
+    let driveSpeedValue = $state(0);
+    let driveDirectionValue = $state(0);
+
+    $effect(() => {
+        if (!isConnected()) goto("/");
+    });
+</script>
+
+<Wrapper connectButtonAction={performDisconnect} connectButtonDisabled={false} connectButtonText={"Disconnect"} statusColor={"text-success"} statusText={"Connected"}>
+    <label for="customRange3" class="form-label">Driving Speed: {driveSpeedValue}</label>
+    <input type="range" class="form-range" min="-100" max="100" bind:value={driveSpeedValue}>
+    <label for="customRange3" class="form-label">Driving Direction: {driveDirectionValue}</label>
+    <input type="range" class="form-range" min="-100" max="100" bind:value={driveDirectionValue}>
+</Wrapper>
